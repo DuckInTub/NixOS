2366c8be8fe0f6487905a421e24c72107891f5ad
DuckInTub <icebl0804@gmail.com>
2025-07-22 21:06:24 +0200

    Added nextcloud

--- hosts/isak-server/configuration.nix
+++ hosts/isak-server/configuration.nix
@@ -1,31 +1,42 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/bootloader.nix
-      ../../modules/locale.nix
-      # ../../modules/networking.nix
-      ../../modules/packages.nix
-      ../../modules/services.nix
-      ../../modules/user.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/bootloader.nix
+    ../../modules/locale.nix
+    # ../../modules/networking.nix
+    ../../modules/packages.nix
+    ../../modules/services.nix
+    ../../modules/user.nix
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Enable networking through networkmanager
   networking.networkmanager.enable = true;
 
-  # Configure a link-local IP on ethernet
-  networking = {
-	useDHCP = true;
-  };
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  # Tailscale, e-books, media-server, pi-hole, nextcloud, overleaf-ce
+
+  services = {
+    nextcloud = {
+      enable = true;
+      hostName = "nextcloud.isak-server.local";
+      autoUpdateApps = true;
+      config.adminpassFile = "/etc/nextcloud-admin-pass";
+    };
+    tailscale.enable = true;
+    jellyfin.enable = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -34,5 +45,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
