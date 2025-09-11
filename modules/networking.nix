237aa09f77cce29b1b02b7e7bb3421d73b856ae7
DuckInTub <icebl0804@gmail.com>
2025-09-11 10:48:26 +0200

    Removed link-local networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,25 +2,8 @@
   # Enable networking through networkmanager
   networking.networkmanager.enable = true;
 
-  # Configure a link-local IP on ethernet
-  networking = {
-    interfaces.enp5s0 = {
-      ipv4.addresses = [
-        {
-          address = "169.254.1.60";
-          prefixLength = 16;
-        }
-      ];
-    };
-  };
-
-  # Add known link-local host for arch PC
-  networking.hosts = {
-    "169.254.1.1" = ["arch"];
-  };
-
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
