0ecc00d6a1b2bc49cc266f3b4d698ce134e10e48
DuckInTub <icebl0804@gmail.com>
2025-10-11 22:52:55 +0200

    Fix: wireguard mtu

--- hosts/isak-server/configuration.nix
+++ hosts/isak-server/configuration.nix
@@ -58,6 +58,7 @@ in {
       enable = true;
       interfaces = {
         wg0 = {
+          mtu = 1380;
           dynamicEndpointRefreshSeconds = 300;
           ips = ["10.10.0.1/24"];
           listenPort = 51820;
@@ -94,61 +95,6 @@ in {
       enable = true;
       domainsFile = "/etc/duckdns/domain";
       tokenFile = "/etc/duckdns/token";
-    };
-    copyparty = {
-      enable = true;
-      # directly maps to values in the [global] section of the copyparty config.
-      # see `copyparty --help` for available options
-      settings = {
-        i = "0.0.0.0";
-        # use lists to set multiple values
-        p = [3210 3211];
-        # use booleans to set binary flags
-        no-reload = true;
-        # using 'false' will do nothing and omit the value when generating a config
-        ignored-flag = false;
-      };
-
-      # create users
-      accounts = {
-        # specify the account name as the key
-        isak = {
-          # provide the path to a file containing the password, keeping it out of /nix/store
-          # must be readable by the copyparty service user
-          passwordFile = "/run/keys/copyparty/isak_password";
-        };
-      };
-
-      # create a volume
-      volumes = {
-        # create a volume at "/" (the webroot), which will
-        "/" = {
-          # share the contents of "/srv/copyparty"
-          path = "/srv/copyparty";
-          # see `copyparty --help-accounts` for available options
-          access = {
-            # everyone gets read-access, but
-            r = "*";
-            # users "ed" and "k" get read-write
-            rw = ["isak"];
-          };
-          # see `copyparty --help-flags` for available options
-          flags = {
-            # "fk" enables filekeys (necessary for upget permission) (4 chars long)
-            fk = 4;
-            # scan for new files every 60sec
-            scan = 60;
-            # volflag "e2d" enables the uploads database
-            e2d = true;
-            # "d2t" disables multimedia parsers (in case the uploads are malicious)
-            d2t = true;
-            # skips hashing file contents if path matches *.iso
-            nohash = "\.iso$";
-          };
-        };
-      };
-      # you may increase the open file limit for the process
-      openFilesLimit = 8192;
     };
 
     nextcloud = {
